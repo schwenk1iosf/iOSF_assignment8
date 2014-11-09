cc21034f07000f359f8f769ec1f44983500055d5
Dustin Schwenk <drschwenk@gmail.com>
2014-11-08 23:22:45 -0800

    first commit

---
+++ Class_Roster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Class_Roster
+//
+//  Created by schwenk on 11/8/14.
+//  Copyright (c) 2014 schwenk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Class_Roster/myPerson.swift
@@ -0,0 +1,9 @@
+//
+//  myPerson.swift
+//  Class_Roster
+//
+//  Created by schwenk on 11/8/14.
+//  Copyright (c) 2014 schwenk. All rights reserved.
+//
+
+import Foundation
