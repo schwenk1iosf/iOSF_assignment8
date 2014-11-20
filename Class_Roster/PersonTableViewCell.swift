62e6d87e542d001f83549e3abd68dc8461501da4
Dustin Schwenk <drschwenk@gmail.com>
2014-11-19 19:10:49 -0800

    Updated through PS 7.

---
+++ Class_Roster/PersonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonTableViewCell.swift
+//  Class_Roster
+//
+//  Created by schwenk on 11/17/14.
+//  Copyright (c) 2014 schwenk. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
